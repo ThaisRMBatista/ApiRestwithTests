13cf3c234406d2294f38ec50250376c1e071a17c
Thais Regina Mendes Batista <thais.batista@maximatech.com.br>
2023-08-21 13:29:47 -0300

    Create interface UserRepository

---
+++ src/main/java/com/apirest/api/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.apirest.api.repositories;
+
+import com.apirest.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
