deeb106283a9d1890a42606a5da1ef1def8de359
Thais Regina Mendes Batista <thais.batista@maximatech.com.br>
2023-08-21 15:29:25 -0300

    Endpoint create for user

--- src/main/java/com/apirest/api/services/UserService.java
+++ src/main/java/com/apirest/api/services/UserService.java
@@ -1,6 +1,7 @@
 package com.apirest.api.services;
 
 import com.apirest.api.domain.User;
+import com.apirest.api.domain.dto.UserDTO;
 
 import java.util.List;
 
@@ -9,4 +10,5 @@ public interface UserService {
 
     User findUserById(Long id);
     List<User> findAll();
+    User create(UserDTO obj);
 }
--- src/main/java/com/apirest/api/services/impl/UserServiceImpl.java
+++ src/main/java/com/apirest/api/services/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.apirest.api.services.impl;
 
 import com.apirest.api.domain.User;
+import com.apirest.api.domain.dto.UserDTO;
 import com.apirest.api.repositories.UserRepository;
 import com.apirest.api.services.UserService;
 import com.apirest.api.services.exceptions.ObjectNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,9 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
     public User findUserById(Long id) {
         Optional<User> userById = this.repository.findById(id);
@@ -24,5 +29,10 @@ public class UserServiceImpl implements UserService {
 
     public List<User> findAll() {
         return this.repository.findAll();
+    }
+
+    @Override
+    public User create(UserDTO obj) {
+        return repository.save(mapper.map(obj, User.class));
     }
 }
