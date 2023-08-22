50547b3d30a43bc7fe31a8fe33ef7684c8b69df4
Thais Regina Mendes Batista <thais.batista@maximatech.com.br>
2023-08-21 21:30:37 -0300

    Test user service class for update method with DataIntegrityViolationException

--- src/test/java/com/apirest/api/services/impl/UserServiceImplTest.java
+++ src/test/java/com/apirest/api/services/impl/UserServiceImplTest.java
@@ -136,6 +136,14 @@ class UserServiceImplTest {
 
     @Test
     void whenUpdateThenADataIntegrityViolationException() {
+        when(repository.findByEmail(anyString())).thenThrow(new DataIntegrityViolationException(E_MAIL_JA_CADASTRADO_NO_SISTEMA));
+
+        try {
+            service.create(userDTO);
+        } catch (Exception ex) {
+            assertEquals(DataIntegrityViolationException.class, ex.getClass());
+            assertEquals(E_MAIL_JA_CADASTRADO_NO_SISTEMA, ex.getMessage());
+        }
     }
 
     @Test
