27c7a52b06ebe6736c430a56659003f562080329
Thais Regina Mendes Batista <thais.batista@maximatech.com.br>
2023-08-22 08:37:17 -0300

    Test controller exception for object not found

---
+++ src/test/java/com/apirest/api/controllers/exceptions/ControllerExceptionHandlerTest.java
@@ -0,0 +1,47 @@
+package com.apirest.api.controllers.exceptions;
+
+import com.apirest.api.services.exceptions.ObjectNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class ControllerExceptionHandlerTest {
+
+    public static final String USUARIO_NAO_ENCONTRADO = "Usuário não encontrado";
+
+    @InjectMocks
+    private ControllerExceptionHandler controllerExceptionHandler;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void whenObjectNotFoundExceptionThenReturnAResponseEntity() {
+        ResponseEntity<StandardError> response = controllerExceptionHandler.objectNotFound(
+                new ObjectNotFoundException(USUARIO_NAO_ENCONTRADO),
+                new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(USUARIO_NAO_ENCONTRADO, response.getBody().getErrors());
+        assertEquals(404, response.getBody().getStatus());
+    }
+
+    @Test
+    void dataIntegrityViolation() {
+    }
+}
