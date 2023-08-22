e316ba8c353e16b57ea2b0d3f19e23ce4fa215b0
Thais Regina Mendes Batista <thais.batista@maximatech.com.br>
2023-08-21 21:34:40 -0300

    Test user service class for delete method with success

--- src/test/java/com/apirest/api/services/impl/UserServiceImplTest.java
+++ src/test/java/com/apirest/api/services/impl/UserServiceImplTest.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class UserServiceImplTest {
@@ -147,7 +147,12 @@ class UserServiceImplTest {
     }
 
     @Test
-    void delete() {
+    void whenDeleteWithSuccess() {
+        when(repository.findById(anyLong())).thenReturn(optionalUser);
+        doNothing().when(repository).deleteById(anyLong());
+
+        service.delete(ID);
+        verify(repository, times(1)).deleteById(anyLong());
     }
 
     private void startUser() {
